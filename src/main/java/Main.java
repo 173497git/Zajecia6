56643a95b552b907f6abc0df4e46c1d137dd94d3
173497git <161622376+173497git@users.noreply.github.com>
2024-06-05 15:18:36 +0000

    6_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,50 @@
-/*
-Program sprawdza poprawność wpisywanego imienia. W przypadku wystąpienia spacji w imieniu, funkcja wyrzuca zdefiniowany wyjątek WrongStudentName, który jest wyłapywany w pętli głównej Commit6_0.
-Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces modyfikacji ogólnie może wyglądać następująco:
-• Ustalenie jaki błąd chcę się sprawdzić i wyłapać.
-• Decyzja, czy użyje się własnej klasy wyjątku, czy wykorzysta już istniejące (np. Exception, IOException).
-• Napisanie kodu sprawdzającego daną funkcjonalność. W przypadku warunku błędu wyrzucany będzie wyjątek: throw new WrongStudentName().
-• W definicji funkcji, która zawiera kod wyrzucania wyjątku dopisuje się daną nazwę wyjątku, np. public static String ReadName() throws WrongStudentName.
-• We wszystkich funkcjach, które wywołują powyższą funkcję także należy dopisać, że one wyrzucają ten wyjątek – inaczej program się nie skompiluje.
-• W pętli głównej, w main’ie, w zdefiniowanym już try-catch dopisuje się Nazwę wyjątku i go obsługuje, np. wypisuje w konsoli co się stało.
-*/
-
-//Commit6_1. Na podstawie analogii do wyjątku WrongStudentName utwórz i obsłuż wyjątki WrongAge oraz WrongDateOfBirth. 
-//Niepoprawny wiek – gdy jest mniejszy od 0 lub większy niż 100. Niepoprawna data urodzenia – gdy nie jest zapisana w formacie DD-MM-YYYY, np. 28-02-2023.
-
 import java.io.IOException;
 import java.util.Scanner;
-import java.io.IOException;
+class WrongStudentName extends Exception {
+    public WrongStudentName(String message) {
+        super(message);
+    }
+}
 
-class WrongStudentName extends Exception { }
+class WrongAge extends Exception {
+    public WrongAge(String message) {
+        super(message);
+    }
+}
 
-class Main {
+class WrongDateOfBirth extends Exception {
+    public WrongDateOfBirth(String message) {
+        super(message);
+    }
+}
+public class Main {
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        while(true) {
+        while (true) {
             try {
                 int ex = menu();
-                switch(ex) {
-                    case 1: exercise1(); break;
-                    case 2: exercise2(); break;
-                    case 3: exercise3(); break;
-                    default: return;
+                switch (ex) {
+                    case 1:
+                        exercise1();
+                        break;
+                    case 2:
+                        exercise2();
+                        break;
+                    case 3:
+                        exercise3();
+                        break;
+                    default:
+                        return;
                 }
-            } catch(IOException e) {
-
-            } catch(WrongStudentName e) {
-                System.out.println("Błędne imie studenta!");
+            } catch (IOException e) {
+                System.out.println("IOException occurred!");
+            } catch (WrongStudentName e) {
+                System.out.println("Błędne imię studenta: " + e.getMessage());
+            } catch (WrongAge e) {
+                System.out.println("Błędny wiek studenta: " + e.getMessage());
+            } catch (WrongDateOfBirth e) {
+                System.out.println("Błędna data urodzenia studenta: " + e.getMessage());
             }
         }
     }
@@ -49,42 +59,58 @@ class Main {
     }
 
     public static String ReadName() throws WrongStudentName {
-        scan.nextLine();
-        System.out.println("Podaj imie: ");
+        scan.nextLine(); 
+        System.out.println("Podaj imię: ");
         String name = scan.nextLine();
-        if(name.contains(" "))
-            throw new WrongStudentName();
-
+        if (name.contains(" ")) {
+            throw new WrongStudentName("Imię nie może zawierać spacji.");
+        }
         return name;
     }
 
-    public static void exercise1() throws IOException, WrongStudentName {
-        var name = ReadName();
+    public static int ReadAge() throws WrongAge {
         System.out.println("Podaj wiek: ");
-        var age = scan.nextInt();
-        scan.nextLine();
-        System.out.println("Podaj datę urodzenia DD-MM-YYY");
-        var date = scan.nextLine();
+        int age = scan.nextInt();
+        if (age < 0 || age > 100) {
+            throw new WrongAge("Wiek musi być w przedziale 0-100.");
+        }
+        return age;
+    }
+
+    public static String ReadDateOfBirth() throws WrongDateOfBirth {
+        scan.nextLine(); 
+        System.out.println("Podaj datę urodzenia (DD-MM-YYYY): ");
+        String date = scan.nextLine();
+        if (!date.matches("^\\d{2}-\\d{2}-\\d{4}$")) {
+            throw new WrongDateOfBirth("Data urodzenia musi być w formacie DD-MM-YYYY.");
+        }
+        return date;
+    }
+
+    public static void exercise1() throws IOException, WrongStudentName, WrongAge, WrongDateOfBirth {
+        var name = ReadName();
+        var age = ReadAge();
+        var date = ReadDateOfBirth();
         (new Service()).addStudent(new Student(name, age, date));
     }
 
     public static void exercise2() throws IOException {
         var students = (new Service()).getStudents();
-        for(Student current : students) {
-            System.out.println(current.ToString());
+        for (Student current : students) {
+            System.out.println(current.toString());
         }
     }
 
     public static void exercise3() throws IOException {
-        scan.nextLine();
-        System.out.println("Podaj imie: ");
+        scan.nextLine(); 
+        System.out.println("Podaj imię: ");
         var name = scan.nextLine();
         var wanted = (new Service()).findStudentByName(name);
-        if(wanted == null)
+        if (wanted == null)
             System.out.println("Nie znaleziono...");
         else {
             System.out.println("Znaleziono: ");
-            System.out.println(wanted.ToString());
+            System.out.println(wanted.toString());
         }
     }
 }
